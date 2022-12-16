a4ac649a1adc980661417efe1dc020ad5b2f5678
miriam128 <miriam.amaral@as3engenharia.com.br>
2022-12-15 21:50:42 -0300

    primeiro commit

---
+++ database/migrations/2022_12_15_203143_create_agendamentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendamentos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('paciente_id')->constrained('paciente')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('medico_id')->constrained('medico')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendamentos');
+    }
+};
