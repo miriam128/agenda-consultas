0db3379394264338f973f52afde2f83ccc4c344d
miriam128 <miriam.amaral@as3engenharia.com.br>
2022-12-19 16:38:37 -0300

    run yarn build

---
+++ public/build/assets/ApplicationLogo-e9609194.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-e488d326.js";const s="/build/assets/BELTSeguros_horiz_roxo-1b72377d.png";function r({className:t}){return o("img",{src:s,width:80,height:80})}export{r as A};
